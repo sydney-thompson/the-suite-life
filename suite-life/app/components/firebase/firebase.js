85e6dd650c5236f6a34341af0071c432b9202ed4
Jessica Pan <jessica@Jessicas-MacBook-Pro.local>
2021-03-01 15:19:10 -0500

    attempt to add stuff

--- suite-life/app/components/firebase/firebase.js
+++ suite-life/app/components/firebase/firebase.js
@@ -10,7 +10,9 @@ if (!firebase.apps.length) {
 }
 
 export const auth = firebase.auth();
+export const db = firebase.database();
 
+// authorization
 export const registerWithEmail = (email, password) => {
     auth.createUserWithEmailAndPassword(email, password);
 }
@@ -26,3 +28,37 @@ export const logout = () => {
 export const passwordReset = (email) => {
     auth.sendPasswordResetEmail(email);
 }
+
+// writing data
+export const updatePerson = (userId, name, email, pronouns, suite_tag) => {
+    db.ref('users/' + userId).set({
+        name: name,
+        email: email,
+        pronouns: pronouns,
+        suite_tag: suite_tag
+    });
+}
+
+export const updateSuite = (chores, transactions, messages, persons, suite_tag) => {
+    db.ref('suites/' + suiteId).set({
+        chores: chores,
+        transactions: transactions,
+        messages: messages,
+        persons: persons,
+        suite_tag: suite_tag
+    });
+}
+
+// reading data
+// export const readPerson = (userId) => {
+//     db.child("users").child(userId).get().then(function(snapshot) {
+//         if(snapshot.exists()) {
+//             console.log(snapshot.val());
+//         }
+//         else {
+//             console.log("No data available");
+//         }
+//     }).catch(function(error) {
+//         console.error(error);
+//     });
+// }
