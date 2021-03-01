9098d6f0d47736d548acbc84319560e506f1c5d1
Sydney Thompson <sydney.thompson@yale.edu>
2021-03-01 18:31:41 -0500

    Fixing bug by adding useState import

--- suite-life/App.js
+++ suite-life/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import * as firebase from "firebase";
 import * as Facebook from "expo-facebook";
@@ -14,37 +14,37 @@ export default function App() {
   const [isImageLoading, setImageLoadStatus] = useState(false);
 
   // listen for authentication state to change
-  auth.onAuthStateChanged(user => {
+  auth.onAuthStateChanged((user) => {
     if (user != null) {
-      console.log('We are authenticated now!');
+      console.log("We are authenticated now!");
     }
   });
 
   async function loginWithFacebook() {
-    await Facebook.initializeAsync('1097367824036534');
-  
+    await Facebook.initializeAsync("1097367824036534");
+
     const { type, token } = await Facebook.logInWithReadPermissionsAsync({
-      permissions: ['public_profile'],
+      permissions: ["public_profile"],
     });
-  
-    if (type === 'success') {
+
+    if (type === "success") {
       // Build Firebase credential with the Facebook access token.
       const credential = auth.FacebookAuthProvider.credential(token);
-  
+
       // Sign in with credential from the Facebook user.
-      auth
-        .signInWithCredential(credential)
-        .catch(error => {
-          console.log("Sign-in error");
-          // Handle Errors here.
-        });
+      auth.signInWithCredential(credential).catch((error) => {
+        console.log("Sign-in error");
+        // Handle Errors here.
+      });
 
       // NOTE: Can retrieve ID with (await response.json()).id or variations like const { id, name } = await response.json();
-      const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-      Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      );
+      Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
     }
   }
-  
+
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
