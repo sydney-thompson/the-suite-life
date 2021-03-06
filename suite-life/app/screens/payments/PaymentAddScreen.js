71df7642d5d54e8a360c978b996f2dc5648c2378
lgeggleston <lia.eggleston@yale.edu>
2021-03-06 10:58:24 -0500

    Basic shell for add payment screen

--- suite-life/app/screens/payments/PaymentAddScreen.js
+++ suite-life/app/screens/payments/PaymentAddScreen.js
@@ -2,18 +2,33 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppButton from "../../components/AppButton";
 import AppText from "../../components/AppText";
+import TextInput2 from "../../components/TextInput2";
 import Screen from "../../components/Screen";
 
 import defaultStyles from "../../config/styles";
 
 export default function PaymentAddScreen() {
+  const AddTransaction = () => {
+    // Placeholder: read values of TextInputs and handle data
+    // navigate back to main payments screen
+  }
+
   return (
     <Screen style={styles.screen}>
-      <AppText style={defaultStyles.title}>Add Payment</AppText>
+      <AppText style={defaultStyles.title}>New Transaction</AppText>
+      <TextInput2 label="Title: " default_text="" ></TextInput2>
+      <TextInput2 label="Amount: " default_text="$0.00" ></TextInput2>
       <AppButton
-        title="Add Payment"
+        title="Select suite members who owe >>>>>>>>"
         color="primary"
-        onPress={() => console.log("Add Payment Tapped")}
+        onPress={() => console.log("Signal for pop-up selection menu")}
+      ></AppButton>
+      <View style={styles.container}>
+      </View>
+      <AppButton
+        title="Save"
+        color="secondary"
+        onPress={() => AddTransaction()}
       ></AppButton>
     </Screen>
   );
@@ -21,6 +36,18 @@ export default function PaymentAddScreen() {
 
 const styles = StyleSheet.create({
   screen: {
+    alignItems: "center",
+  },
+  container: {  // container for suite member icons
+    backgroundColor: defaultStyles.colors.white,
+    borderStyle: "solid",
+    borderColor: defaultStyles.colors.black,
+    borderWidth: 2,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "90%",
+    padding: 30,
+    marginVertical: 1,
     alignItems: "center",
   },
 });
