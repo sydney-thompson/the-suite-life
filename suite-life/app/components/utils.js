95d442997575f687c348da0b95119d649d626ce9
Sydney Thompson <sydney.thompson@yale.edu>
2021-03-13 11:29:15 -0500

    Removing extraneous utils file

--- suite-life/app/components/utils.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-export function rejectDelay(reason) {
-  const t = 500;
-  return new Promise((resolve, reject) => {
-    setTimeout(reject.bind(null, reason), t);
-  });
-}
