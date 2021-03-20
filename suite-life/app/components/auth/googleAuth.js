1a6d2d263a136b9f22c88743b017bc3686a0500d
Sydney Thompson <sydney.thompson@yale.edu>
2021-03-20 10:49:17 -0400

    Adding local login persistence

--- suite-life/app/components/auth/googleAuth.js
+++ suite-life/app/components/auth/googleAuth.js
@@ -7,7 +7,6 @@ import * as firebase from "firebase";
 import { auth } from "../firebase/firebase";
 import firebaseConfig from "../firebase/firebaseConfig";
 
-
 export async function googleLogin() {
   const { type, accessToken, user } = await Google.logInAsync({
     iosClientId: `907884719847-8do0qdtlq6a94oq2q7lijna1cnpkbmqn.apps.googleusercontent.com`,
@@ -15,32 +14,28 @@ export async function googleLogin() {
   });
 
   if (type === "success") {
-
     const credential = firebase.auth.GoogleAuthProvider.credential(
       null,
       accessToken
     );
 
-    auth.signInWithCredential(credential).catch((error) => {
-      // Handle Errors here.
-      console.log("Sign-in error:", error);
-      // Handle Errors here.
-      const errorCode = error.code;
-      
-      //const errorMessage = error.message;
-      // The email of the user's account used.
-      //const email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      const credential = error.credential;
-      if (errorCode === "auth/account-exists-with-different-credential") {
-        Alert.alert("Email already associated with another account.");
-        // Handle account linking here, if using.
-        reject("error");
-      } else {
-        console.error(error);
-        reject("error");
-      }
-      resolve("success");
+    auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(() => {
+      auth.signInWithCredential(credential).catch((error) => {
+        // Handle Errors here.
+        console.log("Sign-in error:", error);
+        // Handle Errors here.
+        const errorCode = error.code;
+        const credential = error.credential;
+        if (errorCode === "auth/account-exists-with-different-credential") {
+          Alert.alert("Email already associated with another account.");
+          // Handle account linking here, if using.
+          reject("error");
+        } else {
+          console.error(error);
+          reject("error");
+        }
+        resolve("success");
+      });
     });
   } else if (type === "cancel") {
     Alert.alert(
