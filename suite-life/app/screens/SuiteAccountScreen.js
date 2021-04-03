54ee1ba070b252d860d2f8c40ae764b2675a4635
Jessica Pan <jessica@Jessicas-MacBook-Pro.local>
2021-04-02 22:08:46 -0400

    Rules screen accessible through suite screen

--- suite-life/app/screens/SuiteAccountScreen.js
+++ suite-life/app/screens/SuiteAccountScreen.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
+import colors from "../config/colors";
+import navigation from "../navigation/RulesNavigator";
+import routes from "../navigation/routes";
 
 import Screen from "../components/Screen";
 import defaultStyles from "../config/styles";
 
-export default function AccountScreen() {
+export default function SuiteAccountScreen({ navigation }) {
   return (
     <Screen style={styles.screen}>
       <View style={styles.container}>
@@ -16,6 +20,16 @@ export default function AccountScreen() {
           <AppText>Suitemate 3</AppText>
         </View>
       </View>
+      <View style={styles.buttonContainer}>
+        <AppButton
+        title="View Rules"
+        color="primary"
+        
+        onPress={() => {
+          navigation.navigate(routes.RULES, {navigation})
+        }}
+        />
+      </View>
     </Screen>
   );
 }
@@ -24,7 +38,6 @@ const styles = StyleSheet.create({
   screen: {
     alignItems: "center",
   },
-
   container: {
     paddingTop: 25,
     alignItems: "center",
@@ -36,4 +49,11 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
   },
   suitematesContainer: {},
+  buttonContainer: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "flex-end",
+    padding: 20,
+    width: "100%",
+   },
 });
