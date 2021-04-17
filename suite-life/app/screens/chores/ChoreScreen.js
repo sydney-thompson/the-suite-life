fa4d34f7807586af8948ed3afce19f96e05f4a69
Sydney Thompson <sydney.thompson@yale.edu>
2021-04-17 18:57:25 -0400

    Updating ChoreScreen to reflect style updates

--- suite-life/app/screens/chores/ChoreScreen.js
+++ suite-life/app/screens/chores/ChoreScreen.js
@@ -18,8 +18,9 @@ import routes from "../../navigation/routes";
 import { disconnectFromChores } from "../../components/firebase/suites";
 import { getUserData } from "../../components/firebase/users";
 import HorizontalSpaceSeparator from "../../components/HorizontalSpaceSeparator";
-import ChoreOverview from "../../components/ChoreOverview";
 import colors from "../../config/colors";
+import Chore from "../../components/Chore";
+import AppTitle from "../../components/AppTitle";
 
 export default function ChoreScreen({ navigation }) {
   const [choreRefresh, setChoreRefresh] = useState("");
@@ -129,45 +130,57 @@ export default function ChoreScreen({ navigation }) {
 
   return (
     <Screen style={styles.screen}>
-      <AppText style={defaultStyles.title}>Chores</AppText>
-      {chores.length === 0 ? (
-        <View style={styles.clearedContainer}>
-          <AppText style={styles.clearedText}>{"All cleared!"}</AppText>
-          <AppText style={styles.clearedText}>{"Good job!"}</AppText>
-        </View>
-      ) : (
-        <FlatList
-          data={chores}
-          keyExtractor={(chore) => chore.id.toString()}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => navigate_to_edit(item)}>
-              <ChoreOverview
-                day={item.day}
-                details={item.details}
-                frequency={item.frequency}
-                name={item.title}
-              />
-            </TouchableOpacity>
-          )}
-          ItemSeparatorComponent={HorizontalSpaceSeparator}
-        />
-      )}
+      <View style={[styles.cardContainer, styles.headerContainer]}>
+        <AppText style={styles.headerText}>Chores</AppText>
+      </View>
+
+      <View style={[styles.cardContainer, { flex: 1 }]}>
+        <AppTitle style={styles.cardText}>{`Balances`}</AppTitle>
+        {chores.length === 0 ? (
+          <View style={styles.clearedContainer}>
+            <AppText style={styles.clearedText}>{"All cleared!"}</AppText>
+            <AppText style={styles.clearedText}>{"Good job!"}</AppText>
+          </View>
+        ) : (
+          <View style={styles.listContainer}>
+            <FlatList
+              data={chores}
+              keyExtractor={(chore) => chore.id.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => navigate_to_edit(item)}>
+                  <Chore
+                    day={item.day}
+                    details={item.details}
+                    frequency={item.frequency}
+                    name={item.title}
+                  />
+                </TouchableOpacity>
+              )}
+              ItemSeparatorComponent={HorizontalSpaceSeparator}
+            />
+          </View>
+        )}
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <AppButton
+          title="Add chore +"
+          color="secondary"
+          onPress={() => navigate_to_add()}
+        ></AppButton>
+      </View>
+
       {/* <FlatList style={{ width: "100%" }}>
         {choresJSON.map((chore) => {
           console.log("chore:", chore);
           <AppButton
-            key={chore.id}
-            color="secondary"
-            title={chore.name + "\n Frequency: " + chore.frequency}
-            onPress={() => navigate_to_edit(chore)}
+          key={chore.id}
+          color="secondary"
+          title={chore.name + "\n Frequency: " + chore.frequency}
+          onPress={() => navigate_to_edit(chore)}
           ></AppButton>;
         })}
       </FlatList> */}
-      <AppButton
-        title="Add Chore"
-        color="primary"
-        onPress={() => navigate_to_add()}
-      ></AppButton>
     </Screen>
   );
 }
@@ -175,7 +188,6 @@ export default function ChoreScreen({ navigation }) {
 const styles = StyleSheet.create({
   screen: {
     alignItems: "center",
-    paddingBottom: 20,
   },
   clearedContainer: {
     flex: 1,
@@ -186,5 +198,44 @@ const styles = StyleSheet.create({
     color: colors.light,
     fontSize: 25,
     fontWeight: "600",
+  },
+  listContainer: {
+    width: "100%",
+  },
+  cardContainer: {
+    backgroundColor: colors.primary,
+    borderRadius: 20,
+    padding: 15,
+    width: "95%",
+    margin: "2%",
+  },
+  headerContainer: {
+    paddingTop: 10,
+    alignItems: "center",
+    backgroundColor: colors.secondary,
+    flex: 0,
+    height: "11%",
+    justifyContent: "center",
+  },
+  cardText: {
+    marginBottom: 10,
+    fontSize: 30,
+    fontWeight: "600",
+  },
+  headerText: {
+    color: colors.white,
+    fontWeight: "500",
+    fontSize: 50,
+  },
+
+  topButtonContainer: {
+    alignItems: "center",
+    marginBottom: -5,
+    width: "95%",
+  },
+
+  buttonContainer: {
+    alignItems: "center",
+    width: "95%",
   },
 });
