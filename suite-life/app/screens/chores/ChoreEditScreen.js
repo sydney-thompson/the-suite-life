285cee0bb08e53ac117ec337885cb4c8be76c53e
Sydney Thompson <sydney.thompson@yale.edu>
2021-04-17 12:41:55 -0400

    Working on chore edit screen for displaying chore values

--- suite-life/app/screens/chores/ChoreEditScreen.js
+++ suite-life/app/screens/chores/ChoreEditScreen.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, ScrollView, Alert } from "react-native";
-import { TextInput } from 'react-native';
+import { TextInput } from "react-native";
 import AppButton from "../../components/AppButton";
 import AppText from "../../components/AppText";
 import Screen from "../../components/Screen";
 import { auth, db } from "../../components/firebase/firebase";
 import * as choreFunctions from "../../components/firebase/chores";
 import * as Yup from "yup";
-import {disconnectFromSuitemates, getSuitemates} from "../../components/firebase/suites"; 
+import {
+  disconnectFromSuitemates,
+  getSuitemates,
+} from "../../components/firebase/suites";
 import { getUserData } from "../../components/firebase/users";
 
 import defaultStyles from "../../config/styles";
@@ -22,56 +25,34 @@ import {
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
   frequency: Yup.string().required().label("Frequency"),
-  details: Yup.string().label("Details")
+  details: Yup.string().label("Details"),
 });
 
 export default function ChoreEditScreen(choreInfo) {
   const [user, setUser] = useState(null);
   const [suitemates, setSuitemates] = useState([]);
   const [initialhousemates, setIHmates] = useState({});
-
-  const [choreName, setChoreName] = useState('')
-  const [choreFrequency, setFrequencyName] = useState('')
-  const [choreAssignees, setChoreAssignees] = useState('')
-  const [choreDetails, setChoreDetails] = useState('')
-  const [choreCompleted, setChoreCompleted] = useState('')
-  const [choreRefresher, setChoreRefresher] = useState('')
+  const [chore, setChore] = useState(choreInfo.route.params.chore);
+  console.log("chore:", chore);
 
   // navigates back to main screen
   const returnHome = () => {
     const navigator = choreInfo.route.params.navigation;
-    navigator.goBack()
-  }
-
-  // gets chore data from firebase and populates text fields with it 
-   const loadChoreData = async () => {
-      if(choreRefresher != "True"){
-        await setChoreRefresher("True")
-        const choreData = await choreFunctions.loadChoreData(choreInfo.route.params.firebaseID)
-        await setChoreName(choreData.name)
-        await setFrequencyName(choreData.frequency)
-        await setChoreAssignees(choreData.assignees)
-        await setChoreDetails(choreData.details)
-        await setChoreCompleted(choreData.completed)
-      }
-  }
-  loadChoreData()
-
-  //const housemates = [{id: 'id1', name: 'Name 1'}, {id: 'id2', name: 'Name 2'}];    // placeholders for reading in the housemates of that suite
-  //const initialhousemates = {'id1': false, 'id2': false};
+    navigator.goBack();
+  };
 
   // deletes chore from firebase then returns to main screen
-const deleteChore = () => {
-  choreFunctions.deleteChore(choreInfo.route.params.firebaseID)
-  returnHome()
-}
+  const deleteChore = () => {
+    choreFunctions.deleteChore(chore.firebaseID);
+    returnHome();
+  };
 
-// sends data to firebase and clears the textbox values 
+  // sends data to firebase and clears the textbox values
   const submitAndClear = async (values) => {
-    await choreFunctions.updateChore(values, choreInfo.route.params.firebaseID)
-    setChoreRefresher("False")
-    returnHome()
-  }
+    await choreFunctions.updateChore(values, choreInfo.route.params.firebaseID);
+    setChoreRefresher("False");
+    returnHome();
+  };
 
   useEffect(() => {
     getUserData().then((val) => {
@@ -86,7 +67,7 @@ const deleteChore = () => {
       let checkvalues = {};
       suitemates.forEach((mate) => {
         checkvalues[mate.id] = false;
-      })
+      });
       setIHmates(checkvalues);
     } else {
       setSuitemates([]);
@@ -103,51 +84,58 @@ const deleteChore = () => {
     <Screen style={styles.screen}>
       <AppText style={defaultStyles.title}>Edit Chore</AppText>
       <Form
-        initialValues={{ name: choreName, frequency: choreFrequency, assignees: initialhousemates, details: choreDetails}}
+        initialValues={{
+          name: choreName,
+          frequency: choreFrequency,
+          assignees: initialhousemates,
+          details: choreDetails,
+        }}
         onSubmit={(values) => submitAndClear(values)}
         validationSchema={validationSchema}
       >
-        <ScrollView style={{width: '100%'}}>
-        <FormField
-          display="AppTextInputLabel"
-          label="Name"
-          autoCapitalize="none"
-          autoCorrect={false}
-          name="name"
-          placeholder="Name of the chore"
-        />
-        <FormField
-          display="AppTextInputLabel"
-          label="Frequency"
-          autoCapitalize="none"
-          autoCorrect={false}
-          name="frequency"
-          placeholder="One-time or weekly?"
-        />
-        <FormField
-          display="AppTextInputLabel"
-          label="Details"
-          autoCapitalize="none"
-          autoCorrect={false}
-          name="details"
-          placeholder="Additional details"
-        />
-        <AppText style={[{color: defaultStyles.colors.black}]}>Select housemates assigned:</AppText>
-        <View>
-        {suitemates.map((mate) => {
-          return (
-            <Checkbox
-              name="assignees"
-              specificName={mate.id}
-              key={mate.id}
-              checkedIcon='check-box'
-              iconType='material'
-              uncheckedIcon='check-box-outline-blank'
-              title={mate.name}
-            />
-          );
-        })}
-        </View>
+        <ScrollView style={{ width: "100%" }}>
+          <FormField
+            display="AppTextInputLabel"
+            label="Name"
+            autoCapitalize="none"
+            autoCorrect={false}
+            name="name"
+            placeholder="Name of the chore"
+          />
+          <FormField
+            display="AppTextInputLabel"
+            label="Frequency"
+            autoCapitalize="none"
+            autoCorrect={false}
+            name="frequency"
+            placeholder="One-time or weekly?"
+          />
+          <FormField
+            display="AppTextInputLabel"
+            label="Details"
+            autoCapitalize="none"
+            autoCorrect={false}
+            name="details"
+            placeholder="Additional details"
+          />
+          <AppText style={[{ color: defaultStyles.colors.black }]}>
+            Select housemates assigned:
+          </AppText>
+          <View>
+            {suitemates.map((mate) => {
+              return (
+                <Checkbox
+                  name="assignees"
+                  specificName={mate.id}
+                  key={mate.id}
+                  checkedIcon="check-box"
+                  iconType="material"
+                  uncheckedIcon="check-box-outline-blank"
+                  title={mate.name}
+                />
+              );
+            })}
+          </View>
         </ScrollView>
         <SubmitButton title="Save Chore" />
       </Form>
@@ -163,7 +151,7 @@ const deleteChore = () => {
       ></AppButton>
     </Screen>
   );
-};
+}
 
 const styles = StyleSheet.create({
   screen: {
--- suite-life/app/screens/chores/ChoreScreen.js
+++ suite-life/app/screens/chores/ChoreScreen.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import { StyleSheet, Text, View, ScrollView  } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import AppButton from "../../components/AppButton";
 import AppText from "../../components/AppText";
 import Screen from "../../components/Screen";
@@ -11,27 +11,28 @@ import defaultStyles from "../../config/styles";
 import routes from "../../navigation/routes";
 
 export default function ChoreScreen({ navigation }) {
-  const [choreRefresh, setChoreRefresh] = useState('')
-  const [choresJSON, setChoresJSON] = useState([])
-  const [vari, setVar] = useState([])
+  const [choreRefresh, setChoreRefresh] = useState("");
+  const [choresJSON, setChoresJSON] = useState([]);
+  const [chores, setChores] = useState([]);
+  const [vari, setVar] = useState([]);
   //var my_suiteID = choreFunctions.get_suiteID();
 
-  // navigate to add screen 
+  // navigate to add screen
   const navigate_to_add = () => {
-    setChoreRefresh("False")
-    navigation.navigate(routes.CHORE_ADD, {navigation})
-  }
+    setChoreRefresh("False");
+    navigation.navigate(routes.CHORE_ADD, { navigation });
+  };
 
-  // navigate to edit screen 
-  const navigate_to_edit = (firebaseID) => {
-    setChoreRefresh("False")
-    navigation.navigate(routes.CHORE_EDIT, {navigation, firebaseID})
-  }
+  // navigate to edit screen
+  const navigate_to_edit = (chore) => {
+    setChoreRefresh("False");
+    navigation.navigate(routes.CHORE_EDIT, { navigation, chore });
+  };
 
-  const renderChores = async() => {
-   // var suiteID = get_suiteID() 
-    var suiteID = await choreFunctions.get_suiteID()
-    await renderChores_helper(suiteID)
+  const renderChores = async () => {
+    // var suiteID = get_suiteID()
+    var suiteID = await choreFunctions.get_suiteID();
+    await renderChores_helper(suiteID);
     //const ss =  await suiteFunctions.getSuitematess('74734846')
     /*
     const suitemateList = await suiteFunctions.getSuitematesList('74734846')
@@ -45,63 +46,71 @@ export default function ChoreScreen({ navigation }) {
     console.log("before")
     console.log(balancesJSON)
     console.log("after")*/
-  }
+  };
 
   const renderChores_helper = async (suiteID) => {
     var choreJSON = [];
-    await db.ref(`/suites/${suiteID}/chores`).once('value', snapshot => {
-       let data = snapshot.val()
-       if(data != "None"){ 
-          let keys = Object.keys(data);
-           // loop through firebase data and add data to choreJSON for use in rendering elements
-           keys.forEach((key) => { 
-             choreJSON.push({name: data[key].name, firebaseID: key, frequency: data[key].frequency})
-           });
-           var holder = choresJSON
-           if(JSON.stringify(holder) != JSON.stringify(choreJSON)){
-             setChoresJSON(choreJSON)
-           }
-       }
-     })
-   }
+    await db.ref(`/suites/${suiteID}/chores`).once("value", (snapshot) => {
+        if (snapshot.exists()) {
+          snapshot.forEach((child) => {
+            choresList.push
+
+      let data = snapshot.val();
+      if (data != "None") {
+        
+        let keys = Object.keys(data);
+        // loop through firebase data and add data to choreJSON for use in rendering elements
+        keys.forEach((key) => {
+          choreJSON.push({
+            name: data[key].name,
+            firebaseID: key,
+            frequency: data[key].frequency,
+          });
+        });
+        var holder = choresJSON;
+        if (JSON.stringify(holder) != JSON.stringify(choreJSON)) {
+          setChoresJSON(choreJSON);
+        }
+      }
+    });
+  };
 
   const refresh_screen = () => {
-    choreJSON = choreFunctions.renderChoress()
-    const holder = choresJSON
-    if(JSON.stringify(holder) != JSON.stringify(choreJSON)){
-        setChoresJSON(choreJSON)
-      }
-  }
+    choreJSON = choreFunctions.renderChoress();
+    const holder = choresJSON;
+    if (JSON.stringify(holder) != JSON.stringify(choreJSON)) {
+      setChoresJSON(choreJSON);
+    }
+  };
 
-  {renderChores()}
+  {
+    renderChores();
+  }
 
   // this causes a re render of the screen when you navigate back to it causing data to refresh
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       // The screen is focused
-      renderChores()
+      renderChores();
     });
     // Return the function to unsubscribe from the event so it gets removed on unmount
     return unsubscribe;
   }, [navigation]);
 
-
-  return ( 
+  return (
     <Screen style={styles.screen}>
       <AppText style={defaultStyles.title}>Chores</AppText>
-      <ScrollView
-        style={{width: '100%'}}>
-         {choresJSON.map((item)=>(
-              <AppButton 
-                key= {item.firebaseID}
-                color="tertiary" 
-                title={ item.name + "\n Frequency: " + item.frequency }
-                onPress={() => navigate_to_edit(item.firebaseID)}> 
-              </AppButton>
-              )
-         )}
-     </ScrollView>
-     <AppButton
+      <ScrollView style={{ width: "100%" }}>
+        {choresJSON.map((item) => (
+          <AppButton
+            key={item.firebaseID}
+            color="tertiary"
+            title={item.name + "\n Frequency: " + item.frequency}
+            onPress={() => navigate_to_edit(item)}
+          ></AppButton>
+        ))}
+      </ScrollView>
+      <AppButton
         title="Add Chore"
         color="primary"
         onPress={() => navigate_to_add()}
