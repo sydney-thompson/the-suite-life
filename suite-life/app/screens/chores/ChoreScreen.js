04142cd5ddab99400a36a03ee22542b8f4ddf52b
Sydney Thompson <sydney.thompson@yale.edu>
2021-04-18 02:11:30 -0400

    cleaning up some old code

--- suite-life/app/screens/chores/ChoreScreen.js
+++ suite-life/app/screens/chores/ChoreScreen.js
@@ -67,64 +67,6 @@ export default function ChoreScreen({ navigation }) {
     navigation.navigate(routes.CHORE_EDIT, { navigation, chore });
   };
 
-  // const renderChores = async () => {
-  //   // var suiteID = get_suiteID()
-  //   var suiteID = await choreFunctions.get_suiteID();
-  //   await renderChores_helper(suiteID);
-  //   //const ss =  await suiteFunctions.getSuitematess('74734846')
-  //   /*
-  //   const suitemateList = await suiteFunctions.getSuitematesList('74734846')
-  //   var balancesJSON = []
-  //   //balancesJSON.keys = suitemateList
-  //   suitemateList.forEach((suitemate) => {
-  //     var new_pair = {suitemate : 0}
-  //     balancesJSON.push(new_pair)
-  //   });
-  //   balancesJSON.keys = suitemateList
-  //   console.log("before")
-  //   console.log(balancesJSON)
-  //   console.log("after")*/
-  // };
-
-  // const renderChores_helper = async (suiteID) => {
-  //   var choreJSON = [];
-  //   await db.ref(`/suites/${suiteID}/chores`).once("value", (snapshot) => {
-  //       if (snapshot.exists()) {
-  //         snapshot.forEach((child) => {
-  //           choresList.push
-
-  //     let data = snapshot.val();
-  //     if (data != "None") {
-
-  //       let keys = Object.keys(data);
-  //       // loop through firebase data and add data to choreJSON for use in rendering elements
-  //       keys.forEach((key) => {
-  //         choreJSON.push({
-  //           name: data[key].name,
-  //           firebaseID: key,
-  //           frequency: data[key].frequency,
-  //         });
-  //       });
-  //       var holder = choresJSON;
-  //       if (JSON.stringify(holder) != JSON.stringify(choreJSON)) {
-  //         setChoresJSON(choreJSON);
-  //       }
-  //     }
-  //   });
-  // };
-
-  // const refresh_screen = () => {
-  //   choreJSON = choreFunctions.renderChoress();
-  //   const holder = choresJSON;
-  //   if (JSON.stringify(holder) != JSON.stringify(choreJSON)) {
-  //     setChoresJSON(choreJSON);
-  //   }
-  // };
-
-  // {
-  //   renderChores();
-  // }
-
   useEffect(() => {
     getUserData().then((val) => {
       setUser(val);
@@ -142,16 +84,6 @@ export default function ChoreScreen({ navigation }) {
       disconnectFromChores();
     };
   }, [user, setChores]);
-
-  // this causes a re render of the screen when you navigate back to it causing data to refresh
-  // useEffect(() => {
-  //   const unsubscribe = navigation.addListener("focus", () => {
-  //     // The screen is focused
-  //     renderChores();
-  //   });
-  //   // Return the function to unsubscribe from the event so it gets removed on unmount
-  //   return unsubscribe;
-  // }, [navigation]);
 
   return (
     <Screen style={styles.screen}>
