c272247a6bb9569a19e15eb7816dc91ee5481a00
Sydney Thompson <sydney.thompson@yale.edu>
2021-04-26 11:26:08 -0400

    adding docstrings to chores.js

--- suite-life/app/components/firebase/chores.js
+++ suite-life/app/components/firebase/chores.js
@@ -1,6 +1,9 @@
 import { auth, db } from "./firebase";
 
 export async function get_suiteID() {
+  /**
+   * Get suite id of the currently logged-in user
+   */
   var uid = auth.currentUser.uid;
   var data = null;
 
@@ -15,6 +18,10 @@ export async function get_suiteID() {
 
 // gets one chore's data from firebase
 export async function loadChoreData(firebaseID) {
+  /**
+   * Load chore {firebaseID} from current user's suite
+   * @param {string} firebaseID   ID of chore to load
+   */
   var suiteID = await get_suiteID();
   var returnData = [];
   await db
@@ -30,25 +37,24 @@ export async function loadChoreData(firebaseID) {
       };
     });
   return returnData;
-
-  /* return new Promise((resolve, reject) => {
-      const ref =  db.ref(`/suites/test123/chores/${firebaseID}`);
-      ref
-        .once("value", (snapshot) => {
-          if (snapshot.exists()) {
-            resolve(snapshot.val());
-          } else {
-            resolve({ code: "chore-not-found" });
-          }
-        })
-        .catch(function (error) {
-          reject(error);
-        });
-    }); */
 }
 
 // function to push new chore to firebase
 export async function addNewChore(info) {
+  /**
+   * Add new chore to the current user's suite
+   * @param {Object} info     Chore details. Should be structured as follows:
+   *        {
+   *            assignees: {
+   *                suitemateID: true/false, (need entry for each suitemate)
+   *            }
+   *            completed: true/false,
+   *            day: string, (from config/daysOfWeek.js)
+   *            details: string,
+   *            name: string,
+   *            recurring: true/false
+   *        }
+   */
   var suiteID = await get_suiteID();
   await db.ref(`/suites/${suiteID}/chores/`).push({
     assignees: info.assignees,
@@ -60,34 +66,14 @@ export async function addNewChore(info) {
   });
 }
 
-// function to push new chore to firebase
-export async function addNewChore1(info) {
-  var suiteID = await get_suiteID();
-  await db.ref(`/suites/${suiteID}/chores`).once("value", (snapshot) => {
-    let data = snapshot.val();
-    if (data == "None") {
-      db.ref("/suites/" + suiteID + "/chores/").set({
-        name: info.name,
-        frequency: info.frequency,
-        assignees: info.assignees,
-        details: info.details,
-        completed: info.completed,
-      });
-      choreFunctions.updateChore(info);
-    } else {
-      db.ref("/suites/${suiteID}/chores/").push({
-        name: info.name,
-        frequency: info.frequency,
-        assignees: info.assignees,
-        details: info.details,
-        completed: info.completed,
-      });
-    }
-  });
-}
-
 // function to update data in firebase
 export function updateChore(chore, choreID, suiteID) {
+  /**
+   * Update {choreID} in suite {suiteID} with details from {chore}
+   * @param {Object} chore    chore with details to update - requires name, recurring, assignees, details, and day
+   * @param {string} choreID  ID of chore to update
+   * @param {string} suiteID  ID of suite containing chore
+   */
   return new Promise((resolve, reject) => {
     const choreUpdate = {
       name: chore.name,
@@ -113,6 +99,11 @@ export function updateChore(chore, choreID, suiteID) {
 }
 
 export function completeChore(choreID, suiteID) {
+  /**
+   * Mark chore {choreID} as complete in suite {suiteID}
+   * @param {string} choreID    ID of chore to mark complete
+   * @param {string} suiteID    ID of suite to search for chore
+   */
   return new Promise((resolve, reject) => {
     const choreUpdate = {
       completed: true,
